ff8fa336165994068b8328ee460d99625dd8089f
sirauronmach3 <cyrilmartinmj@gmail.com>
2025-05-05 12:24:00 -0400

    added header

---
+++ imageLSB.py
@@ -0,0 +1,11 @@
+"""
+imageLSB.py
+This module provides functions to hide and extract messages in images using the least significant bit (LSB) method.
+
+Matthew Martin mjm836
+CS-465-001 SP 24-25
+Assignment 3
+
+"""
+
+import os
